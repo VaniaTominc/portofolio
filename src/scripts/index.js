7908987547450b34a0f15717ffc4e30919ee845c
vtominc <vanja.tominc@gmail.com>
2021-08-23 22:45:09 +0200

    home button not yet working

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,66 +1,72 @@
-// import LocomotiveScroll from 'locomotive-scroll'
+function init() {
 
-// const scroll = new LocomotiveScroll({
-//   el: document.querySelector('[data-scroll-container]'),
-//   smooth: true,
-//   offset: ['30%', 100],
-//   multiplier: 1
-// })
+  // import LocomotiveScroll from 'locomotive-scroll'
 
-// const cursor = document.querySelector('.cursor')
-// document.addEventListener('mousemove', (event) => {
-//   cursor.style.left = event.pageX + 'px'
-//   cursor.style.top = event.pageY + 'px'
-// })
+  // const scroll = new LocomotiveScroll({
+  //   el: document.querySelector('[data-scroll-container]'),
+  //   smooth: true,
+  //   offset: ['30%', 100],
+  //   multiplier: 1
+  // })
+
+  // const cursor = document.querySelector('.cursor')
+  // document.addEventListener('mousemove', (event) => {
+  //   cursor.style.left = event.pageX + 'px'
+  //   cursor.style.top = event.pageY + 'px'
+  // })
 
 
 
-let darkMode = localStorage.getItem('darkMode')
+  let darkMode = localStorage.getItem('darkMode')
 
-const darkModeToggle = document.querySelector('#dark-mode-toggle')
+  const darkModeToggle = document.querySelector('#dark-mode-toggle')
 
-const enableDarkMode = () => {
-  document.body.classList.add('darkmode')
-  // Updating darkMode in localStorage
-  localStorage.setItem('darkMode', 'enabled')
-}
-
-const disableDarkMode = () => {
-  document.body.classList.remove('darkmode')
-  // Updating darkMode in localStorage 
-  localStorage.setItem('darkMode', null)
-}
- 
-// Enabled darkMode from previous visit, therefore start things off with it on
-if (darkMode === 'enabled') {
-  enableDarkMode()
-}
-
-// Toggle function for dark/light
-darkModeToggle.addEventListener('click', () => {
-  darkMode = localStorage.getItem('darkMode')
-  
-  // if currently not enabled, enable it
-  if (darkMode !== 'enabled') {
-    enableDarkMode()
-  // if enabled, turn it off  
-  } else {  
-    disableDarkMode() 
+  const enableDarkMode = () => {
+    document.body.classList.add('darkmode')
+    // Updating darkMode in localStorage
+    localStorage.setItem('darkMode', 'enabled')
   }
-})
 
+  const disableDarkMode = () => {
+    document.body.classList.remove('darkmode')
+    // Updating darkMode in localStorage 
+    localStorage.setItem('darkMode', null)
+  }
+ 
+  // Enabled darkMode from previous visit, therefore start things off with it on
+  if (darkMode === 'enabled') {
+    enableDarkMode()
+  }
 
-// Scrolling home button
-const scrollingHomeButton = document.querySelector('.scroll-home')
-
-window.addEventListener('scroll', () => {
-  scrollingHomeButton.classList.toggle('active', window.scrollY > 600)
-})
-
-function scrollToTop() {
-  window.scrollTo({
-    top: 0,
-    behavior: 'smooth'
+  // Toggle function for dark/light
+  darkModeToggle.addEventListener('click', () => {
+    darkMode = localStorage.getItem('darkMode')
+  
+    // if currently not enabled, enable it
+    if (darkMode !== 'enabled') {
+      enableDarkMode()
+      // if enabled, turn it off  
+    } else {  
+      disableDarkMode() 
+    }
   })
+
+
+  // Scrolling home button
+  const scrollingHomeButton = document.querySelector('.scroll-home')
+
+  window.addEventListener('scroll', () => {
+    scrollingHomeButton.classList.toggle('active', window.scrollY > 600)
+  })
+
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    })
+  }
+
+
 }
 
+window.addEventListener('DOMContentLoaded', init)
